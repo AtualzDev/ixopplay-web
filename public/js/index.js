fae5d232c101b8720f47256eaa46cbbaff0bad81
ataliasraniel <atalias.herculys@gmail.com>
2022-01-27 12:38:17 -0300

    finish slider and nav bar fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,50 @@
-let navCircleInside = document.querySelector('.nav-circle-inside');
+let slideIndex = 1;
+showSlides();
 
-
-function showInsideCircle() {
-    navCircleInside.setAttribute('style', 'display: block');
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slider-img");
+    const sliderFades = document.querySelectorAll(".slider-fade");
+    sliderFades.forEach((fade) => {
+        fade.classList.remove("slider-fade")
+    })
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].classList.add("slider-fade");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    
+    setTimeout(showSlides, 6000)
 }
+
+//SECTIONS CHECK
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav-bar .nav-item-holder li");
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id");
+            console.log(current)
+        }
+    });
+    
+    
+    navLi.forEach((li) => {
+        li.classList.remove("active-li");
+    })
+    if(current == 'know-us'){
+        navLi.item(0).classList.add("active-li");
+    }else if (current == 'consumers'){
+        navLi.item(1).classList.add("active-li");
+    }
+
+
+    
+};
